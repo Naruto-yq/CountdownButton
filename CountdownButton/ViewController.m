0d8cb32c1be23a338f8a90a9a123675df9953ba5
余钦 <yuqin@yuqindeiMac.local>
2016-03-17 20:51:08 +0800

    first commit

--- CountdownButton/ViewController.m
+++ CountdownButton/ViewController.m
@@ -7,9 +7,10 @@
 //
 
 #import "ViewController.h"
+#import "UICountdownButton.h"
 
 @interface ViewController ()
-
+@property(nonatomic, weak)UICountdownButton *countdownBtn;
 @end
 
 @implementation ViewController
@@ -17,6 +18,24 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    self.view.backgroundColor = [UIColor whiteColor];
+    [self SetupSubviews];
+}
+
+- (void)SetupSubviews
+{
+    UICountdownButton *countdownBtn = [[UICountdownButton alloc]init];
+    countdownBtn.frame = CGRectMake(100, 100, 150, 50);
+    countdownBtn.backgroundColor = [UIColor redColor];
+    countdownBtn.second = 5;
+    [countdownBtn addTarget:self action:@selector(ClickCountdownBtn:) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:countdownBtn];
+    self.countdownBtn = countdownBtn;
+}
+
+- (void)ClickCountdownBtn:(UICountdownButton *)btn
+{
+    [btn start];
 }
 
 - (void)didReceiveMemoryWarning {
